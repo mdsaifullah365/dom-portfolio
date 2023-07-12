c834844a0ebb464329d9f8698e8aae92c55ba1e7
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-07-12 18:27:26 +0600

    add tech-stack section

---
+++ js/icon.js
@@ -0,0 +1,38 @@
+const html = '../img/icons/html.svg';
+const css = '../img/icons/css.svg';
+const javascript = '../img/icons/javascript.svg';
+const react = '../img/icons/react.svg';
+const nextjs = '../img/icons/next-js.svg';
+const bootstrap = '../img/icons/bootstrap.svg';
+const expressjs = '../img/icons/express-js.svg';
+const git = '../img/icons/git.svg';
+const github = '../img/icons/github-white.svg';
+const mongodb = '../img/icons/mongodb.svg';
+const mongoose = '../img/icons/mongoose.svg';
+const nodejs = '../img/icons/node-js.png';
+const reactnative = '../img/icons/react-native.svg';
+const redux = '../img/icons/redux.svg';
+const sass = '../img/icons/sass.svg';
+const tailwindcss = '../img/icons/tailwind-css.svg';
+const typescript = '../img/icons/typescript.svg';
+const vscode = '../img/icons/vs-code.svg';
+
+export {
+  html,
+  css,
+  javascript,
+  react,
+  nextjs,
+  bootstrap,
+  expressjs,
+  git,
+  github,
+  mongodb,
+  mongoose,
+  nodejs,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  vscode,
+};
