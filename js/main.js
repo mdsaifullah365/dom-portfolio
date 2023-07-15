0d63005ed23e3e58088aefb224006a9a565f8369
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-07-15 22:13:04 +0600

    refactor main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ import sidebar from './sidebar.js';
 import techStack from './tech.js';
 
 const body = document.body;
+
 const main = document.createElement('main');
 main.classList.add('content');
 main.setAttribute('id', 'home');
 
 body.append(sidebar);
 body.append(main);
+
 main.append(hero);
 main.append(about);
 main.append(techStack);
